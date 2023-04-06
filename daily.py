ed94fca219b004a2f7d99ae5f4859f0ed768a828
YohanWD <yohan.wuidar@cartesoft.lu>
2023-04-06 13:49:11 +0200

    remove print + add check if the html already exist do not update db + add graph of activity + purge old html file

--- daily.py
+++ daily.py
@@ -1,9 +1,12 @@
 from utils.scraping import *
-import logging, sys, os
+import logging, sys, os, glob
 from dotenv import load_dotenv
 
 from db_functions.db_funct import *
 from utils.members_fct import *
+
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 def main():
     # Get variable from environnement
@@ -19,8 +22,10 @@ def main():
     db_name = 'test.db' # os.getenv('DB_NAME')
     discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
 
+    already_updated = False
     # Download the page if wasn't downloaded
-    html_file_path = "tmp_dir/" + squad_name + "_" + str(datetime.today().strftime('%Y-%m-%d')) + ".html"
+    html_file_name = squad_name + "_" + str(datetime.today().strftime('%Y-%m-%d')) + ".html"
+    html_file_path = "tmp_dir/" + html_file_name
     if os.path.exists(html_file_path) == False:
         if download_web_page(squad_url,html_file_path) == False:
             logging.info("Error during the page download")
@@ -28,36 +33,49 @@ def main():
         else:
             logging.debug("HTML page was downloaded")
     else:
-        logging.debug("HTML page already downloaded")
-
-    # Scrap the page
-    new_squad_members_list = correct_email_protection(scrap_squadron_profile_page(html_file_path),
-                        list_of_all_members(html_file_path))
-    
-    # print(new_squad_members_list[90].pseudo)
-    
-    # Compare with data in database
-    db_squad_list = get_all_squad_members(db_name)
-    # print(db_squad_list[90].pseudo)
-    list_create_squad, list_to_update, list_leaver  = compare_squads_members(db_squad_list,new_squad_members_list)
-    # print("to delete",list_leaver)
-    # print("to create", list_create_squad)
-    # print("to update", list_to_update)
+        logging.debug("HTML page already downloaded, skipping insert in DB")
+        already_updated = True
 
     # Update database
-    update_squad_members_activity(db_name,list_to_update)
-    insert_all_squad(db_name,list_create_squad)
-    delete_list_of_members(db_name,list_leaver) # Keep an history somewhere ? # TODO#TOTHINK
+    if already_updated == False:
+        # Scrap the page
+        new_squad_members_list = correct_email_protection(scrap_squadron_profile_page(html_file_path),
+                            list_of_all_members(html_file_path))
+        # Compare with data in database
+        db_squad_list = get_all_squad_members(db_name)
+        list_create_squad, list_to_update, list_leaver  = compare_squads_members(db_squad_list,new_squad_members_list)
+
+        update_squad_members_activity(db_name,list_to_update)
+        insert_all_squad(db_name,list_create_squad)
+        delete_list_of_members(db_name,list_leaver) # Keep an history somewhere ? # TODO#TOTHINK
     
-    # Check if we need to warn inactive members 
+    # Check if we need to warn inactive members
     for el in get_all_squad_members(db_name):
         if check_if_members_is_inactive(el):
             msg = f"The following members {el.pseudo} is inactive for more than 3 weeks"
             send_discord_notif(discord_webhook_url,msg) # exluce new player ?
     
     # Generate graph
- 
-    # Delete old HTML file
+    for el in get_all_squad_members(db_name):
+        history_list = get_activity_history_from_members(db_name, el.id)
+        x_list = []
+        y_list = []
+        for elem1, elem2 in history_list:
+            x_list.append(datetime.strptime(elem2, '%Y-%m-%d %H:%M:%S'))
+            y_list.append(elem1)
 
+        plt.clf()
+        plt.xlabel('x - time')
+        plt.ylabel('y - activity')
+        plt.plot(x_list, y_list, color='green', linestyle='dashed', linewidth = 3,
+                marker='o', markerfacecolor='blue', markersize=12)
+
+        plt.gcf().autofmt_xdate()
+        plt.title(f'Activity plot for {el.pseudo}')
+        plt.savefig(f'tmp_dir/graph/{el.pseudo}.png')
+
+    # Delete old HTML file
+    purge('tmp_dir',f"{squad_name}_.*.html",html_file_name)
+    
 if __name__=="__main__":
     main()
